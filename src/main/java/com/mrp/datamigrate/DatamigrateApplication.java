96f18b40e9b96b4f83cd5269fbe719f2a577db37
Matheus Peluchi <matheuspeluchi@gmail.com>
2023-05-08 15:19:10 -0400

    first commit

---
+++ src/main/java/com/mrp/datamigrate/DatamigrateApplication.java
@@ -0,0 +1,13 @@
+package com.mrp.datamigrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamigrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamigrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrp/datamigrate/DatamigrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrp.datamigrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamigrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
